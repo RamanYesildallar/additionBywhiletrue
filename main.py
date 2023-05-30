545b7281ec7f3616c8cc7618806da1928fd1b8cf
Ramazan Yeşildallar <rayesildallar@gmail.com>
2023-05-30 10:14:42 +0300

    firts code for github

---
+++ main.py
@@ -0,0 +1,21 @@
+def adDition():
+    myList = []
+
+
+
+
+
+    while True:
+
+        num=int(input("numbers:"))
+        myList.append(num)
+        result = 0
+        if num==0:
+            for i in myList:
+                result+=i
+            print(result)
+            break
+
+
+# For using code using function
+adDition()
